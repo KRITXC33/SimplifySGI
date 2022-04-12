9b53b4c3cb9031125652346d178147ae4b2a566a
KRTC <79935756+KRTCDev@users.noreply.github.com>
2022-04-12 15:35:02 -0700

    Add files via upload

---
+++ components/shop.js
@@ -0,0 +1,64 @@
+(function(){ 
+let $ = "weapons"
+const Weapons = 'rifle, sniper, shotgun, melee, pistol'
+let weapons = prompt(`Which weapons are availible?, rifle, sniper, shotgun, melee, pistol`)
+alert(`${weapons} are availible`); [1,2,3].forEach("alert"); + alert("Which weapons would you like to buy?"); + alert('Would you like to open the shop now?') + [1,2,3,4].forEach('alert)')
+//availible weapons, plus prompt to open shop
+
+let load = "ammo"
+const ammo = "9mm, .45, .22, shells"
+let loadit = prompt(`Which ammo do you choose?, 9mm, .45, .22, shells`)
+alert(`${loadit} are you options.`)
+[1,2,3,4].forEach('alert')
+//ammo choices
+
+
+})()
+
+(function(){
+    'use strict'
+let $ = "knife"
+const dmg = "damage"
+ dmg = "50" & any 
+ let str = Knife
+ const b = boost
+alert('Player now has boost!')[1,2].forEach('alert')
+//if the player has a boost, then raise damage
+ alert('If player has boost, raise damage'); [1,2,3].forEach('alert')
+ let nb = "no boost"
+ alert(Boolean(nb)); 
+ 
+})()
+
+(function(){
+const $ = "shop"
+$ = 'skins, addons, wraps'
+shopisopen = true(Boolean(shopisopen));
+let shopItems = prompt(`What are the contents?, skins, addons, wraps`) 
+console.log(`${shopItems}`)
+ //shop area of items
+ 
+
+
+})()
+//currencies now
+(function(){
+    'use strict'
+const defcur = "Shop"
+ArmoryisUp = true(Boolean(ArmoryisUp)); + alert('Shop for currencies now while the shop is online'), [1,2,3].forEach('alert')
+let defcur1 = prompt(`What are the deals?, 3000 for $14.99`)
+console.log(`${defcur1}`)
+
+
+
+
+
+
+})()
+let err = null
+const undefined = shopError
+let shopError = true(Boolean(shopError)); console.log('Error in shop, please try again').forEach('alert')
+const shopFix = true
+shopFix = true(Boolean(shopFix)); console.log('Error resolved, proceed now...')
+//shop error resolve
+

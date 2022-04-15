c27422d2641332cc1019a412faa56be82b27fb91
KRTC <79935756+KRTCDev@users.noreply.github.com>
2022-04-15 15:51:45 -0700

    Add files via upload

---
+++ fps/components/agecheck.js
@@ -0,0 +1,13 @@
+(function(){
+    let entAccess;
+    let age = prompt('Please input your age', '') 
+    if (age > 13) {
+        entAccess = true;
+    }else{
+        entAccess = false;
+    }
+    alert(entAccess)
+    const yearsOld = 18
+    useryearsOld = true(Boolean(useryearsOld));
+    console.log('user is 13'),[1,2].forEach('alert')
+})()
